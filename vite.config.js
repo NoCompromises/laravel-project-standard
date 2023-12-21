4acbd2023901fbb80287bda974ffb2202fa91201
Joel Clermont <joel@nocompromises.io>
2023-12-21 17:47:10 -0600

    tweak editor and eslint config, bump js packages, js lint

--- vite.config.js
+++ vite.config.js
@@ -6,45 +6,45 @@ const path = require("path");
 const fs = require("fs");
 
 export default defineConfig(({ mode }) => {
-    const config = {
-        plugins: [
-            laravel({
-                input: ["resources/css/app.css", "resources/js/app.js"],
-            }),
-            vue(),
-        ],
-        resolve: {
-            alias: {
-                "~public": path.resolve(__dirname, "public"),
-                "@js": path.resolve(__dirname, "resources/js"),
-            },
-        },
-        test: {
-            environment: "jsdom",
-            coverage: {
-                reporter: ["text", "html"],
-                reportsDirectory: path.resolve(
-                    __dirname,
-                    "resources/js/tests/coverage"
-                ),
-            },
-        },
+  const config = {
+    plugins: [
+      laravel({
+        input: ["resources/css/app.css", "resources/js/app.js"],
+      }),
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        "~public": path.resolve(__dirname, "public"),
+        "@js": path.resolve(__dirname, "resources/js"),
+      },
+    },
+    test: {
+      environment: "jsdom",
+      coverage: {
+        reporter: ["text", "html"],
+        reportsDirectory: path.resolve(
+          __dirname,
+          "resources/js/tests/coverage"
+        ),
+      },
+    },
+  };
+
+  if (mode === "development") {
+    config.server = {
+      port: 30098,
+      strictPort: true,
+      host: true,
+      https: {
+        key: fs.readFileSync("docker/nginx/key.pem"),
+        cert: fs.readFileSync("docker/nginx/ssl.pem"),
+      },
+      hmr: {
+        host: "project.domain",
+      },
     };
+  }
 
-    if (mode === "development") {
-        config.server = {
-            port: 30098,
-            strictPort: true,
-            host: true,
-            https: {
-                key: fs.readFileSync("docker/nginx/key.pem"),
-                cert: fs.readFileSync("docker/nginx/ssl.pem"),
-            },
-            hmr: {
-                host: "project.domain",
-            },
-        };
-    }
-
-    return config;
+  return config;
 });
